351b7a99d0d8aa8b43e3d0b8d356e4988d0e18cd
Gabriela <gabriela@MacBook-Pro-de-Gabriela.local>
2020-06-03 22:27:21 -0300

    # conceitos

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,31 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, request } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = ['Diego', 'Gabriela', 'Ted'];
+
 app.get('/users', (req: Request, res: Response) => {
-  res.json(['Diego', 'Gabriela', 'Ted']);
+  const search = String(req.query.search);
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+  res.json(filteredUsers);
+});
+
+app.get('/users/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  const user = users[id];
+
+  return res.json(user);
+});
+
+app.post('/users', (req: Request, res: Response) => {
+  const data = req.body;
+  const user = data;
+  return res.json(user);
 });
 
 app.listen('3333', () => {
-
-})
+  console.log('server working')
+});
